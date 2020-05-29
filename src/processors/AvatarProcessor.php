c349fcf14f6d382e641694d91cf1fc4b1487b7d8
ginda chagaduy <chagaduy@hotmail.com>
2020-05-29 11:04:12 +0700

    no message

--- src/processors/AvatarProcessor.php
+++ src/processors/AvatarProcessor.php
@@ -6,7 +6,7 @@ class AvatarProcessor {
     public static function get( $name, $image = null, $folder = "bageur", $type = "initials") {
         if (empty($image)) {
             if (!empty($name)) {
-                return 'https://avatars.dicebear.com/v2/'.$type.'/' . preg_replace('/[^a-z0-9 _.-]+/i', '', $name) . '.svg?options[radius]=50';
+                return 'https://avatars.dicebear.com/v2/'.$type.'/' . preg_replace('/[^a-z0-9 _.-]+/i', '', $name) . '.svg';
             }
             return null;
         }
