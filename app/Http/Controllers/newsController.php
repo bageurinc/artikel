fbfdb672f63d589890c47fbbf8d388b3e69a25e7
Adnan Bayu <adnan.bayufirmansyah@gmail.com>
2020-11-10 18:09:13 +0700

    Adding : struktur news(artikel) dan artikel detail

--- app/Http/Controllers/newsController.php
+++ app/Http/Controllers/newsController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Bageur\Artikel\model\artikel;
 
 class newsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('news');
+        $request['sort_by'] = 'created_at.desc';
+        $artikel = artikel::datatable($request);
+        return view('news',compact('artikel'));
+    }
+    public function detailartikel($id,$any)
+    {
+        $find    = artikel::findOrfail($id);
+        return view('detail-artikel',compact('find'));
     }
 }
