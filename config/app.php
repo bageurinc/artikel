4880789bd404972e4ab8d7dfb0e377f3e97763af
adnanfirmansyah <adnanbayu1998@gmail.com>
2020-11-18 11:51:22 +0700

    PSR-4 Setting

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Bageur\PaketUmrah\PaketUmrahServiceProvider::class,
+        Bageur\Inventory\InventoryServiceProvider::class,
 
         /*
          * Package Service Providers...
