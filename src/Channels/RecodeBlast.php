5a0e90331b4338af0ed8d623ac2507e8e74f9d63
Naufal <naufalrandialdiansyah@gmail.com>
2021-09-03 20:34:55 +0700

    hapus limit user

--- src/Channels/RecodeBlast.php
+++ src/Channels/RecodeBlast.php
@@ -10,7 +10,7 @@ class RecodeBlast
     public function send ($notifiable, Notification $notification) {
         try {
                 $message  = $notification->toBlast($notifiable);
-                $data = config('bageur.auth.artikel_blast')::limit(3)->get();
+                $data = config('bageur.auth.artikel_blast')::get();
                 foreach ($data as $key => $value) {
                     // Mail::to('404.ginda@gmail.com')
                     //         ->queue(new \Bageur\Artikel\Mail\Blast($message['data']));
